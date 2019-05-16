ab27cc09fb01e28496aaec777b420ac729ae46a2
Hayden Chester <ches8516@stthomas.edu>
2019-05-16 16:32:55 -0500

    added compare chart documentation

--- CSPay/R/compareChart.R
+++ CSPay/R/compareChart.R
@@ -1,6 +1,14 @@
-
+#' Title
+#'
+#' @param school
+#' @param spread
+#' @param min
+#' @param max
+#'
+#' @return
+#' @export
+#'
+#' @examples
 compareChart <- function(school, spread = 5, min = TRUE, max = TRUE){
-
-
 
 }
