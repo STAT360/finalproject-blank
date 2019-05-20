69fe135869208dad967dca7d2e1ed1625244f41a
Hayden Chester <ches8516@stthomas.edu>
2019-05-20 14:53:57 -0500

    Added lil jon beep functions and blank documentation.

---
+++ CSPay/R/beeps.R
@@ -0,0 +1,31 @@
+library(here)
+library(beepr)
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+beepyeah <- function(){
+  beep(here("Audio", "yeeeeeeeah.wav"))
+}
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+beepwhat <- function(){
+  beep(here("Audio", "lil-jon-what.wav"))
+}
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+beepokay <- function(){
+  beep(here("Audio", "lil-jon-okay.wav"))
+}
