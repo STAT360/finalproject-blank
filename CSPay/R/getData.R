e12a6291cf1ac568f648c7c9582e76d20d4295d0
Hayden Chester <ches8516@stthomas.edu>
2019-05-20 15:55:01 -0500

    Rename/refactor

--- CSPay/R/getData.R
+++ CSPay/R/getData.R
@@ -4,7 +4,7 @@
 #' @return
 #'
 #' @examples
-getData <- function(url="https://www.payscale.com/college-salary-report/best-schools-by-majors/computer-science"){
+getData <- function(url="https://www.payscale.com/college-salary-report/best-schools-by-majors/computer-science", numberOfPages=30){
   library(stringr)
   library(readr)
   library(rvest)
@@ -15,52 +15,47 @@ getData <- function(url="https://www.payscale.com/college-salary-report/best-sch
 
   url_base <- url
 
-  map_df(1:30, function(i) {
+  map_df(1:numberOfPages, function(i) {
 
     cat(".")
 
-    pg <- read_html(sprintf(url_base, i))
-
+    pg <- read_html(paste0(url_base, i))
     data.frame(pay_low=html_text(html_nodes(pg, ".text-center")))
 
   }) -> income
 
-  map_df(1:30, function(i) {
+  beepwhat()
+
+  map_df(1:numberOfPages, function(i) {
 
     cat(".")
 
-    pg <- read_html(sprintf(url_base, i))
+    pg <- read_html(paste0(url_base, i))
 
     data.frame(School_Name=html_text(html_nodes(pg, ".datatable-logo")))
 
-  }) -> by_school
+  }) -> pay_by_school
 
-  beep()
   ##################------- PART 2-------#########################
   money<-income %>% filter(str_detect(pay_low, "\\$")) %>% mutate(pay_low=parse_number(as.character(pay_low))) %>% pull(pay_low)
 
-  name <- by_school$School_Name
+  name <- pay_by_school$School_Name
 
   low_pay <- c()
 
-  beep()
+
   ##################------- PART 3-------#########################
   answer=c()
   answer2=c()
   for (i in 1:length(money)){
     if ((Mod(i)%%2) == 1) {
       answer[ceiling(i/2)]<-money[i]
-      #append(low_pay, income$pay_low[i])
-      #income$pay_low[i]
     }
     else{
       answer2[i/2]<-money[i]
     }
   }
-  by_school$low_pay=answer
-  by_school$high_pay=answer2
-
-  beep("/CSpay/Audio/yeeeeeeeah.wav") #needs to work on a relative path level
-
+  pay_by_school$low_pay=answer
+  pay_by_school$high_pay=answer2
 
 }
