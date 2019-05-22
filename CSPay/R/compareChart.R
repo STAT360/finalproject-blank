1d9688ec2a3e2cd3c7e71141aa711463a4d2f3ef
Hayden Chester <ches8516@stthomas.edu>
2019-05-22 14:32:54 -0500

    Rename

--- CSPay/R/compareChart.R
+++ CSPay/R/compareChart.R
@@ -1,14 +1,48 @@
-#' Title
+library(forcats)
+library(ggplot2)
+
+#' Title: GetPayFunction
 #'
-#' @param school
-#' @param spread
-#' @param min
-#' @param max
-#'
+#' @param school Name
 #' @return
-#' @export
 #'
-#' @examples
-compareChart <- function(school, spread = 5, min = TRUE, max = TRUE){
+#' @examples: getGraph(University of St Thomas (Minnesota))
+
+compareChart <- function(school='University of St Thomas (Minnesota)',whichPay="low_pay"){
+  if(length(which(pay_by_school$School_Name==school))==0){
+    stop("Error: School is not found")
+  }
+  if(identical(whichPay,"low_pay")==FALSE&&identical(whichPay,"high_pay")==FALSE){
+    stop("Error: pay category is not found")
+  }
+  # if(whichPay=="low_pay"){
+  #
+  # }
+
+  schoolPosition <- pay_by_school %>%
+    arrange(desc(!! sym(whichPay)))
+
+  lowvalue <- which(schoolPosition$School_Name == school)-5
+  highvalue <- which(schoolPosition$School_Name == school)+5
+  includetop <- 1
+  includebottom <- dim(pay_by_school)[1]
+
+  if(lowvalue<0){
+    lowvalue <- 1
+    includetop <- NA
+  }
+  if(highvalue>includebottom){
+    includebottom <- NA
+  }
+
+  schoolPosition <- schoolPosition %>%
+    slice(includetop, lowvalue:highvalue, includebottom)
+  schoolPosition <- schoolPosition %>%
+    mutate(colhighlight = if_else(School_Name == school, TRUE, FALSE))
+  schoolPosition <- schoolPosition %>%
+    mutate(School_Name = fct_reorder(School_Name, !! sym(whichPay)))
+
+  ggplot(schoolPosition) + geom_col(aes(x=School_Name, y=!! sym(whichPay), fill=colhighlight)) +  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +coord_flip()
 
 }
+
